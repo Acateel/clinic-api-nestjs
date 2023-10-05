21bbf7dbbac5172b67fb7c23a58b23a9ab105f45
alexander-dobrin <alexander.dobrin@icloud.com>
2023-10-05 22:31:03 +0300

    Fix: make profile endpoint follow common style.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -49,7 +49,12 @@ export class UserController {
     return this.userService.get();
   }
 
-  @Get('profile')
+  // REVIEW: ты на митинге приводил примеры ендпоинтов:
+  // => /courses/list/filters
+  // => /courses/unregistered
+  // Скорее всего (я додумываю) есть и ендпоинт /courses/:id
+  // как тогда различать когда передают id, а когда "unregistered", условием?
+  @Get(':current/profile')
   @UseGuards(JwtAuthGuard, new RolesGuard(UserRoleEnum.ADMIN))
   @ApiBearerAuth()
   @ApiOperation({ summary: 'admin' })
